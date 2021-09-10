d480306b35ad6e5c4d709896dd1071200326f09a
Elin Larsson <elin.larsson@schibsted.com>
2021-09-10 16:38:02 +0200

    inital commit

---
+++ src/Company.ts
@@ -0,0 +1,29 @@
+import faker from "faker";
+import { Mappable } from "./CustomMap";
+
+export class Company implements Mappable {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+      <div class="info-window">
+        <h1>Company ${this.companyName}</h1>
+        <p><b>Our moto:</b> ${this.catchPhrase}</p>
+      </div>
+    `;
+  }
+}
